5e4f6d95a8e04861d3d01e0744b0fb69c4924d32
felipeal15 <felipeal3@al.insper.edu.br>
2026-08-18 07:58:56 -0300

    Terminando o handout 1, tinha esquecido de commitar

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+
 def extract_route(request : str):
   linha_request = request.split()
   caminho_com_barra = linha_request[1]
@@ -5,23 +7,42 @@ def extract_route(request : str):
   return route
 
 
-
-request = """ 
-GET /img/logo-getit.png HTTP/1.1
-Host: 0.0.0.0:8080
-Connection: keep-alive
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36
-Accept: image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8
-Referer: http://0.0.0.0:8080/
-Accept-Encoding: gzip, deflate
-Accept-Language: en-US,en;q=0.9,pt;q=0.8
-"""
-
-extract_route(request)
-
-
 def read_file(filepath):
     file = open(filepath, "rb") #APENAS O B NAO FUNCIONAVA, TEM QUE SER RB (read, binary) ; Definindo como o arquivo será aberto
     content = file.read() #efetivamente lê o conteudo, tem que abrir, depois ler
     file.close()
     return content #TAMBEM TEM COMO UTILIZAR O WITH, QUE GARANTE QUE O ARQUIVO SERÁ FECHADO
+
+
+def load_data(nome_json):
+   arquivo = open(f"data/{nome_json}") #Por padrão é o "r"
+   dados = json.load(arquivo)
+   arquivo.close()
+
+   return dados
+
+
+def load_template(template):
+  arquivo = open(f"templates/{template}", "r")
+  dados_template = arquivo.read()
+  arquivo.close()
+
+  return dados_template
+
+
+def add_note(nota):
+    notas = load_data('notes.json')
+    notas.append(nota)
+
+    arquivo = open('data/notes.json', 'w')
+    json.dump(notas, arquivo, ensure_ascii=False, indent=2)
+    arquivo.close()
+
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    response = f'HTTP/1.1 {code} {reason}\n'
+    if headers:
+        response += f'{headers}\n'
+    response += f'\n{body}'
+
+    return response.encode()
